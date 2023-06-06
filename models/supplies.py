ecac47943b6a2258a4f3b38fd46904102e8ef748
KevinSerna <kevinsernap@gmail.com>
2023-06-06 06:56:43 -0500

    create models Supplier and supplies

---
+++ models/supplies.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Float, String,ForeignKey, Date, Boolean
+from config.database import Base
+from sqlalchemy.orm import relationship
+
+
+
+class Supplies(Base):
+    __tablename__= "Supplies"
+
+    id= Column(Integer, primary_key=True)
+    supplier_id= Column(Integer, ForeignKey("Supplier.id"))
+    product_id= Column (Integer, ForeignKey("Product.id"))
+    purchase_price= Column(Float)
